a1aced5d1952f7ece6593c2e5e4e2c2a0a84e30a
QianYicheng <qsp2020@outlook.com>
2019-08-07 19:29:19 +0800

    GuiCamera moudle

---
+++ src/com/GuiCamera/test.java
@@ -0,0 +1,9 @@
+package com.GuiCamera;
+
+public class test {
+    public static void main(String[] args)
+    {
+        GuiCamera cam= new GuiCamera("d:\\Hello", "png");//
+        cam.snapShot();
+    }
+}
